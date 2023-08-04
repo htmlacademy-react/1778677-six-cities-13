780f79e67a59efa871b9827fc0bc69cf087cea91
Alina Mangusheva <alinamr@mail.ru>
2023-08-04 21:37:40 +0300

    feat: добавлен оранжевый маркер

--- src/const.ts
+++ src/const.ts
@@ -75,9 +75,9 @@ const CITIES_LOCATION : CityOffer[] = [
 ];
 
 
-const URL_MARKER_DEFAULT = 'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/pin.svg';
+const URL_MARKER_DEFAULT = 'img/pin.svg';
 
-const URL_MARKER_CURRENT = 'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/main-pin.svg';
+const URL_MARKER_CURRENT = 'img/pin-active.svg';
 
 export { AppRoute, AuthorizationStatus, URL_MARKER_DEFAULT, URL_MARKER_CURRENT, BlockName, STARTS_COUNT, CITIES_LOCATION };
 

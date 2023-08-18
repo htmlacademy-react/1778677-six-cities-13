b464a286bea27f99862eb9048ec3306046660c09
Alina Mangusheva <alinamr@mail.ru>
2023-08-18 21:27:38 +0300

    fixed: исправлена ошибка в типе Review

--- src/types/review.ts
+++ src/types/review.ts
@@ -1,11 +1,13 @@
-// type User = {
-//   name: string;
-//   avatarUrl: string;
-//   isPro: boolean;
-// }
+type User = {
+  name: string;
+  avatarUrl: string;
+  isPro: boolean;
+}
 
 export type Review = {
   id: string;
   comment: string;
   rating: number;
+  date: string;
+  user: User;
 };
